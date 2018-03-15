67d8b3dac9de52d28da379cca0d27e5367e9161d
i2rkmen <i2rkmen@gmail.com>
2018-03-14 20:14:43 -0700

    firstpush

---
+++ TruliaTRONE/src/test/java/com/trulia/utilities/Driver.java
@@ -0,0 +1,44 @@
+package com.trulia.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Driver {
+	private static WebDriver driver;
+	
+	private Driver() {}
+
+	public static WebDriver getDriver() {
+		if (driver == null) {
+			switch (ConfigurationReader.getProperty("browser")) {
+			case "firefox":
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+				break;
+			case "chrome":
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+				break;
+			case "safari":
+				WebDriverManager.chromedriver().setup();
+				driver = new SafariDriver();
+				break;
+			default:
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+			}
+		}
+		return driver;
+	}
+
+	public static void closeDriver() {
+		if (driver != null) {
+			driver.quit();
+			driver = null;
+		}
+	}
+}
---
+++ TruliaTRONE/src/test/java/com/trulia/utilities/TestBase.java
@@ -0,0 +1,23 @@
+package com.trulia.utilities;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class TestBase {
+	protected WebDriver driver;
+	
+	@BeforeClass(alwaysRun=true)
+	public void setUp() {
+		driver=Driver.getDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get(ConfigurationReader.getProperty("url"));
+	}
+	@AfterClass
+	public void tearDown() {
+		Driver.closeDriver();
+	}
+
+}
